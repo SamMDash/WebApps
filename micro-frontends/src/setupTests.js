3a74cb9fa2b1782414e977702d56087575a0a957
sammdash <anbessm16@gmail.com>
2026-03-23 17:31:45 +0000

    Add polyfill for ResizeObserver in setupTests.js for jsdom environment

--- micro-frontends/src/setupTests.js
+++ micro-frontends/src/setupTests.js
@@ -1,3 +1,12 @@
 // mock the fetch global using jest
 require("jest-fetch-mock").enableMocks();
 jest.mock("./next-ui/Components/i18n/utils");
+
+// Polyfill ResizeObserver for jsdom/test environment
+if (typeof window !== "undefined" && !window.ResizeObserver) {
+	window.ResizeObserver = class {
+		observe() {}
+		unobserve() {}
+		disconnect() {}
+	};
+}
